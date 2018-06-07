92c31d95ac07285cbc53f8081222f469c34c9a4d
yuchengying <578214130@qq.com>
2018-06-07 23:09:29 +0800

    edit commit

---
+++ src/main/java/com/jdjr/mybatis/TestControler.java
@@ -0,0 +1,13 @@
+package com.jdjr.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
