082ae5f24c2569dbe1167c102b8916cace2c98c6
yuchengying <578214130@qq.com>
2018-06-07 23:00:55 +0800

    yuchengying first commit

---
+++ src/main/java/com/jdjr/mybatis/App.java
@@ -0,0 +1,13 @@
+package com.jdjr.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
